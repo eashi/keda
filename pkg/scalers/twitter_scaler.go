854c30d79f2b1c1317195cdfe8fdb48a1fae96c1
Emad Alashi <emad.ashi@gmail.com>
2019-10-13 18:01:49 +1100

    Add IsActive, ParseMetadata, and constructor

--- pkg/scalers/twitter_scaler.go
+++ pkg/scalers/twitter_scaler.go
@@ -2,7 +2,10 @@ package scalers
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 
+	log "github.com/sirupsen/logrus"
 	"k8s.io/api/autoscaling/v2beta1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -20,6 +23,48 @@ type twitterScaler struct {
 
 type twitterMetadata struct {
 	targetTwitterStatus int
+	accountToMonitor    string
+	accessKey           string
+	accessSecret        string
+}
+
+func NewTwitterScaler(resolvedEnv, metadata map[string]string) (Scaler, error) {
+	meta, err := ParseTwitterScalerMetadata(metadata, resolvedEnv)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing Twitter scaler metadata: %s", err)
+	}
+
+	return &twitterScaler{
+		metadata: meta,
+	}, nil
+}
+
+func ParseTwitterScalerMetadata(metadata, resolvedEnv map[string]string) (*twitterMetadata, error) {
+	meta := twitterMetadata{}
+	meta.targetTwitterStatus = 10
+
+	if val, ok := metadata[twitterMetricName]; ok {
+		tempParsedValue, err := strconv.Atoi(val)
+		if err != nil {
+			log.Errorf("Error parsing twitter scaler metadata %s: %s", twitterMetricName, err.Error())
+			return nil, fmt.Errorf("Error parsing twitter scaler metadata %s: %s", twitterMetricName, err.Error())
+		}
+
+		meta.targetTwitterStatus = tempParsedValue
+	}
+
+	if val, ok := metadata["accountToMonitor"]; ok && val != "" {
+		meta.accountToMonitor = val
+	} else {
+		return nil, fmt.Errorf("no accountToMonitor given")
+	}
+
+	return &meta, nil
+}
+
+func (s *twitterScaler) IsActive(ctx context.Context) (bool, error) {
+	//TODO: give proper implementation to IsActive
+	return true, nil
 }
 
 func (s *twitterScaler) GetMetricSpecForScaling() []v2beta1.MetricSpec {
@@ -42,4 +87,8 @@ func (s *twitterScaler) GetMetrics(ctx context.Context, metricName string, metri
 	}
 
 	return append([]external_metrics.ExternalMetricValue{}, metric), nil
+}
+
+func (s *twitterScaler) Close() error {
+	return nil
 }
