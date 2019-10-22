13f9ffc5689033e171d246bab96914365cf69e05
Emad Alashi <emad.ashi@gmail.com>
2019-10-23 08:20:56 +1100

    Add the basic Twitter API calls

--- pkg/scalers/twitter_scaler.go
+++ pkg/scalers/twitter_scaler.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/dghubble/go-twitter/twitter"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
 	"k8s.io/api/autoscaling/v2beta1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -89,9 +92,30 @@ func (s *twitterScaler) GetMetricSpecForScaling() []v2beta1.MetricSpec {
 func (s *twitterScaler) GetMetrics(ctx context.Context, metricName string, metricSelector labels.Selector) ([]external_metrics.ExternalMetricValue, error) {
 
 	//TODO: Get value from Twitter
+	config := &clientcredentials.Config{
+		ClientID:     s.metadata.accessKey,
+		ClientSecret: s.metadata.accessSecret,
+		TokenURL:     "https://api.twitter.com/oauth2/token",
+	}
+
+	// http.Client will automatically authorize Requests
+	httpClient := config.Client(oauth2.NoContext)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	// user timeline
+	userTimelineParams := &twitter.UserTimelineParams{ScreenName: "golang", Count: 2}
+	tweets, _, _ := client.Timelines.UserTimeline(userTimelineParams)
+	fmt.Printf("USER TIMELINE:\n%+v\n", tweets)
+
+	//TODO: return only the tweets since x based on the parameter since_d
 
 	//TODO: Transform value you got from Twitter and give it the right weight
 	xxx := 5
+	if len(tweets) > 0 {
+		xxx = 10
+	}
 	metric := external_metrics.ExternalMetricValue{
 		MetricName: metricName,
 		Value:      *resource.NewQuantity(int64(xxx), resource.DecimalSI),
